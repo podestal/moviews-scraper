ff79c3a7201e8c41b5cecd0966bc1cbf1f2959c1
podestal <luisrodriguezpodesta@gmail.com>
2024-02-03 15:25:17 -0500

    normalizing strs

--- main.py
+++ main.py
@@ -8,205 +8,20 @@ from chrome_options import ChromeOptions
 from time import sleep
 import pandas as pd
 import json
+import re
+from unicodedata import normalize
+
+def normalize_string(s):
+    s = re.sub(
+            r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
+            normalize( "NFD", s), 0, re.I
+        )
+    s = normalize( 'NFC', s)
+    return s
 
 
-NETFLIX_USERNAME = 'magnus3@VoltVex.com'
-NETFLIX_PASSWORD = 'p433837'
-
-URL = 'https://www.netflix.com/browse/genre/34399?so=az'
 chrome_options = ChromeOptions()
 driver = webdriver.Chrome(options=chrome_options.get_options())
-# driver.get(URL)
-# driver.maximize_window()
-
-# login_link = driver.find_element(By.XPATH, value='//*[@id="appMountPoint"]/div/div[2]/div/div[2]/a')
-# login_link.click()
-
-# sleep(3)
-
-# username_input = driver.find_element(By.ID, value='id_userLoginId')
-# username_input.send_keys(NETFLIX_USERNAME)
-
-# password_input = driver.find_element(By.ID, value='id_password')
-# password_input.send_keys(NETFLIX_PASSWORD)
-
-# sleep(3)
-
-# login_button = driver.find_element(By.XPATH, value='//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
-# login_button.click()
-
-# sleep(3)
-
-# profile_button = driver.find_element(By.XPATH, value='//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[2]/div/div/ul/li[1]/div/a/div/div')
-# profile_button.click()
-
-# sleep(3)
-
-# while True:
-#     prev_heigh = driver.execute_script('return document.body.scrollHeight')
-#     driver.execute_script(f'window.scrollTo(0, document.body.scrollHeight)')
-#     sleep(3)
-#     new_height = driver.execute_script('return document.body.scrollHeight')
-#     if new_height == prev_heigh:
-#         print('breaking')
-#         break
-
-# row_number = 0
-# item_number = 0
-
-# while row_number <= 648:
-#     if item_number == 6:
-#         item_number = 0
-#         row_number += 1
-#     if row_number == 648:
-#         break
-#     title = driver.find_element(By.XPATH, value=f'//*[@id="title-card-{row_number}-{item_number}"]/div[1]/a/div/div/p')
-#     movies[title.text] = 'netflix'
-#     print(title.text)
-#     print(row_number)
-#     print(item_number)
-#     item_number += 1
-
-DISNEY_URL = 'https://www.disneyplus.com/identity/login/enter-email'
-
-# driver.get(DISNEY_URL)
-# driver.maximize_window()
-
-# username = 'l.r.p.2991@gmail.com'
-# password = '13anguloX'
-
-# sleep(10)
-
-# email = driver.find_element(By.ID, value='email')
-# email.send_keys(username)
-
-# continue_button = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div/div/div/div[2]/div/form/button')
-# continue_button.click()
-
-# sleep(10)
-
-# password_input = driver.find_element(By.XPATH, value='//*[@id="password"]')
-# password_input.send_keys(password)
-
-# sleep(10)
-
-# login_button = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div/div/div/div[2]/div/form/button')
-# login_button.click()
-
-# sleep(20)
-
-# For profiles
-
-# profile_button = driver.find_element(By.XPATH, value='//*[@id="remove-main-padding_index"]/div/div/section/ul/div[1]/div/div')
-# profile_button.click()
-
-# sleep(20)
-
-# driver.get('https://www.disneyplus.com/en-gb/movies/9f7c38e5-41c3-47b4-b99e-b5b3d2eb95d4')
-
-# sleep(20)
-
-# while True:
-#     prev_heigh = driver.execute_script('return document.body.scrollHeight')
-#     driver.execute_script(f'window.scrollTo(0, document.body.scrollHeight)')
-#     sleep(5)
-#     new_height = driver.execute_script('return document.body.scrollHeight')
-#     if new_height == prev_heigh:
-#         print('breaking')
-#         break
-
-# all_titles = driver.find_elements(By.XPATH, value='//*[@id="section_index"]/article/div[2]/div/div/div/div/div')
-
-# item_number = 1
-# total_titles = len(all_titles)
-
-# while item_number <= total_titles:
-#     title = driver.find_element(By.XPATH, value=f'//*[@id="section_index"]/article/div[2]/div/div/div/div/div[{item_number}]/a/div')
-#     movies[title.get_attribute('alt')] = 'disney'
-#     item_number += 1
-
-
-# paramount
-
-# PARAMOUNT_URL = 'https://www.paramountplus.com/movies/'
-
-# USERNAME = 'cuentastreaming29010@gmail.com'
-# PASSWORD = 'zavastech100290'
-
-# driver.get(PARAMOUNT_URL)
-# driver.maximize_window()
-
-# Credentials scraping
-
-# username_input = driver.find_element(By.XPATH, value='//*[@id="email"]')
-# username_input.send_keys(USERNAME)
-
-# password_input = driver.find_element(By.XPATH, value='//*[@id="password"]')
-# password_input.send_keys(PASSWORD)
-
-# sleep(10)
-
-# submit = driver.find_element(By.XPATH, value='//*[@id="sign-in-form"]/div/div[3]/button')
-# submit.click()
-
-# sleep(10)
-
-# profile = driver.find_element(By.XPATH, value='//*[@id="who-s-watching"]/ul/li[5]/div[1]/div')
-# profile.click()
-
-# while True:
-#     prev_heigh = driver.execute_script('return document.body.scrollHeight')
-#     driver.execute_script(f'window.scrollTo(0, document.body.scrollHeight)')
-#     sleep(5)
-#     new_height = driver.execute_script('return document.body.scrollHeight')
-#     if new_height == prev_heigh:
-#         print('breaking')
-#         break
-
-# item_number = 1
-# all_titles = driver.find_elements(By.XPATH, value='//*[@id="main-container"]/section[1]/div/article')
-
-# while item_number <= len(all_titles):
-#     title = driver.find_element(By.XPATH, value=f'//*[@id="main-container"]/section[1]/div/article[{item_number}]/a/div/img')
-#     movies[title.get_attribute('alt')] = 'paramount'
-#     item_number += 1
-
-# PRIME_URL = 'https://www.justwatch.com/pe/proveedor/amazon-prime-video?monetization_types=flatrate'
-
-# driver.get(PRIME_URL)
-# driver.maximize_window()
-
-# sleep(5)
-
-# while True:
-#     prev_heigh = driver.execute_script('return document.body.scrollHeight')
-#     driver.execute_script(f'window.scrollTo(0, document.body.scrollHeight)')
-#     sleep(5)
-#     new_height = driver.execute_script('return document.body.scrollHeight')
-#     if new_height == prev_heigh:
-#         print('breaking')
-#         break
-
-# item_number = 1
-# all_titles = driver.find_elements(By.XPATH,value='//*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div')
-
-# while item_number < len(all_titles):
-#     try:
-#         title = driver.find_element(By.XPATH, value=f'//*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[{item_number}]/a/span/div/picture/img')
-#         movies[title.get_attribute('alt')] = 'prime'
-#     except:
-#         print('error')
-#     item_number += 1
-    
-    # movies[title.get_attribute('alt')] = 'prime'
-# //*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[181]/a/span/div/picture/img
-# //*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[2]/a/span/div/picture/img
-# //*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[2]/a/span/div/picture/img
-# //*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[3]/a/span/div/picture/img
-# //*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[13]/a/span/div/picture/img
-# //*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[73]/a/span/div/picture/img
-
-# total 100
 
 JUSTWATCH_URL = 'https://www.justwatch.com/pe'
 NETFLIX_URL = 'https://www.justwatch.com/pe/proveedor/netflix?monetization_types=flatrate&sort_by=title'
@@ -309,19 +124,12 @@ while item_number < len(all_titles):
         title_element = driver.find_element(By.XPATH, value=f'//*[@id="base"]/div[3]/div/div[2]/div/div[1]/div/div[{item_number}]/a/div/picture/img')
         spanish_title_one = title_element.get_attribute('data-src').split('/')[-1].replace('-', ' ')
         spanish_title_two = title_element.get_attribute('alt')
-
-        # if spanish_movies.get(spanish_title_one):
-        #     print('spanish title exist')
-        # else:
-        #     spanish_movies[spanish_title_one] = spanish_title_two
-        #     spanish_titles.append(f'{spanish_title_one}, {spanish_title_two}')
-        #     print('spanish title does not exist')
         if movies.get(spanish_title_one):
-            movies[spanish_title_one]['spanish_title_one'] = spanish_title_one
-            movies[spanish_title_one]['spanish_title_two'] = spanish_title_two
+            movies[spanish_title_one]['spanish_title_one'] = normalize_string(spanish_title_one)
+            movies[spanish_title_one]['spanish_title_two'] = normalize_string(spanish_title_two)
         elif movies.get(spanish_title_two):
-            movies[spanish_title_two]['spanish_title_one'] = spanish_title_one
-            movies[spanish_title_two]['spanish_title_two'] = spanish_title_two
+            movies[spanish_title_two]['spanish_title_one'] = normalize_string(spanish_title_one)
+            movies[spanish_title_two]['spanish_title_two'] = normalize_string(spanish_title_two)
     except:
         print('error')
     item_number += 1
@@ -494,18 +302,19 @@ while item_number < len(all_titles):
 # my_nested_dict['title']['streaming'].append('disney')
 # print(my_nested_dict)
     
-original_title = []
-title_two = []
-title_three = []
-title_four = []
-streaming = []
+# original_title = []
+# title_two = []
+# title_three = []
+# title_four = []
+# streaming = []
 
-for key in movies:
-    original_title.append(movies[key]['original_title'])
-    title_two.append(key)
-    title_three.append(movies[key]['spanish_title_one'])
-    title_four.append(movies[key]['spanish_title_two'])
-    streaming.append(movies[key]['streaming'])
+# for key in movies:
+#     original_title.append(movies[key]['original_title'])
+#     title_two.append(key)
+#     title_three.append(movies[key]['spanish_title_one'])
+#     title_four.append(movies[key]['spanish_title_two'])
+#     streaming.append(movies[key]['streaming'])
 
 with open("sample.json", "w") as file:
     json.dump(movies, file, indent=4)
+
